f1abb7c00129fc4bfdaf9568541a5ec03df15cea
Nilesh <nileshverma171@gmail.com>
2018-03-20 20:15:24 +0530

    Added form

--- src/components/price-info.js
+++ src/components/price-info.js
@@ -3,9 +3,25 @@ import { connect } from 'react-redux';
 import { fetchBtcPriceAction } from '../actions/fetchBtcPrice';
 
 class PriceInfo extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      amount: '',
+      currency: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({currency: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert(this.state.value);
+    event.preventDefault();
+  }
 
   componentDidMount() {
     this.props.fetchBtcPrice();
@@ -23,6 +39,19 @@ class PriceInfo extends Component {
         <div>
           <span>Default Currency: {this.props.defaultCurrency}</span>
         </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" name="amount" value={this.state.amount} onChange={this.handleChange} />
+          </label>
+          <select value={this.state.currency} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option selected value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     )
   }
